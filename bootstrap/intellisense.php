de50e2fdbd14ca7b87496b0fba2b53fbf4ef1dfe
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-04-11 15:13:21 +0200

    Add bootstrap intellisense * Add intellisense.php

---
+++ bootstrap/intellisense.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------------------------------------------------------
+ *  This file has no purpose other than to trick IDEs to offer intellisense for dynamically aliased classes.
+ * ---------------------------------------------------------------------------------------------------------------------
+ *  Please note that this file should not be loaded in the application!
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+
+
+class App extends \MAKS\Velox\Backend\App {}
+class Config extends \MAKS\Velox\Backend\Config {}
+class Router extends \MAKS\Velox\Backend\Router {}
+class View extends \MAKS\Velox\Frontend\View {}
+class Data extends \MAKS\Velox\Frontend\Data {}
+class Path extends \MAKS\Velox\Frontend\Path {}
+class HTML extends \MAKS\Velox\Frontend\HTML {}
