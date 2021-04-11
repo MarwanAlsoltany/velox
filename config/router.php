2027ad9e707a6c326834cc6f8666227264f030b3
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-04-11 15:06:56 +0200

    Add router config file * Add router.php

---
+++ config/router.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------------------------------------------------------
+ *  This config file can be used to override "Router::class" default parameters.
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+
+
+return [
+
+
+    // The base URL of the application.
+    'base'            => Router::DEFAULTS['base'],
+
+
+    // Whether to allow for multiple route matching or not.
+    'allowMultiMatch' => Router::DEFAULTS['allowMultiMatch'],
+
+
+    // Whether route matching should be case sensitive or not.
+    'caseMatters'     => Router::DEFAULTS['caseMatters'],
+
+
+    // Whether trailing slashes in the route matter or not.
+    'slashMatters'    => Router::DEFAULTS['slashMatters'],
+
+
+];
