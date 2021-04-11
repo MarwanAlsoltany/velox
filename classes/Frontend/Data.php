c3ae1990604e05f51615e06ef49cb5bca6dbacaa
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-04-11 14:59:26 +0200

    Add data class * Add Data.php

---
+++ classes/Frontend/Data.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * @author Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
+ * @copyright Marwan Al-Soltany 2021
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MAKS\Velox\Frontend;
+
+use MAKS\Velox\Backend\Config;
+use MAKS\Velox\Helper\Misc;
+
+/**
+ * A class that serves as an abstracted data bag/store that is accessible via dot-notation.
+ *
+ * Example:
+ * ```
+ * // get all data
+ * $allData = Data::getAll();
+ *
+ * // check for variable availability
+ * $someVarExists = Data::has('someVar');
+ *
+ * // get a specific variable or fall back to a default value
+ * $someVar = Data::get('someVar', 'fallbackValue');
+ *
+ * // set a specific variable value
+ * Data::set('someNewVar.someKey', 'someValue');
+ * ```
+ *
+ * @since 1.0.0
+ * @api
+ */
+class Data
+{
+    /**
+     * The currently loaded data.
+     */
+    protected static ?array $bag;
+
+
+    /**
+     * Loads the data from system configuration.
+     *
+     * @return void
+     */
+    protected static function load(): void
+    {
+        if (!isset(static::$bag)) {
+            static::$bag = Config::get('data', []);
+
+            Config::set('data', null);
+        }
+    }
+
+    /**
+     * Checks whether a value exists in `self::$bag` via dot-notation.
+     *
+     * @param string $key The dotted key representation.
+     *
+     * @return bool
+     */
+    public static function has(string $key): bool
+    {
+        static::load();
+
+        $value = Misc::getArrayValueByKey(self::$bag, $key, null);
+
+        return isset($value);
+    }
+
+    /**
+     * Gets a value of a key from `self::$bag` via dot-notation.
+     *
+     * @param string $key The dotted key representation.
+     * @param mixed $fallback [optional] The default fallback value.
+     *
+     * @return mixed The requested value or null.
+     */
+    public static function get(string $key, $fallback = null)
+    {
+        static::load();
+
+        return Misc::getArrayValueByKey(self::$bag, $key, $fallback);
+    }
+
+    /**
+     * Sets a value of a key from `self::$bag` via dot-notation.
+     *
+     * @param string $key The dotted key representation.
+     * @param mixed $value The value to set.
+     *
+     * @return void
+     */
+    public static function set(string $key, $value): void
+    {
+        static::load();
+
+        Misc::setArrayValueByKey(self::$bag, $key, $value);
+    }
+
+    /**
+     * Returns the currently loaded data.
+     *
+     * @return array
+     */
+    public static function getAll(): ?array
+    {
+        static::load();
+
+        return static::$bag;
+    }
+}
