a8ea7e86aa2cf9153339292a1784db2342237d4f
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-08-10 22:32:21 +0200

    Update globals class * Add $globals static property * Add instance() method * Add __get() magic method * Update class DocBlock

--- classes/Backend/Globals.php
+++ classes/Backend/Globals.php
@@ -33,6 +33,15 @@ use MAKS\Velox\Helper\Misc;
  * @method static static setServer(string $key, $value) Sets a value in $_SERVER. Dot-notation can be used for nested values.
  * @method static static setEnv(string $key, $value) Sets a value in $_ENV. Dot-notation can be used for nested values.
  *
+ * @property object $get A class around the superglobal `$_GET` that has the methods `get()`, `set()`, and `getAll()`.
+ * @property object $post A class around the superglobal `$_POST` that has the methods `get()`, `set()`, and `getAll()`.
+ * @property object $files A class around the superglobal `$_FILES` that has the methods `get()`, `set()`, and `getAll()`.
+ * @property object $cookie A class around the superglobal `$_COOKIE` that has the methods `get()`, `set()`, and `getAll()`.
+ * @property object $session A class around the superglobal `$_SESSION` that has the methods `get()`, `set()`, and `getAll()`.
+ * @property object $request A class around the superglobal `$_REQUEST` that has the methods `get()`, `set()`, and `getAll()`.
+ * @property object $server A class around the superglobal `$_SERVER` that has the methods `get()`, `set()`, and `getAll()`.
+ * @property object $env A class around the superglobal `$_ENV` that has the methods `get()`, `set()`, and `getAll()`.
+ *
  * @since 1.0.0
  */
 final class Globals
@@ -49,6 +58,11 @@ final class Globals
     ];
 
 
+    /**
+     * This array holds an anonymous class that acts as a wrapper for each superglobal.
+     */
+    protected static array $globals;
+
     protected static bool $isInitialized = false;
 
     private static array $_GET;
@@ -60,6 +74,18 @@ final class Globals
     private static array $_SERVER;
     private static array $_ENV;
 
+
+    /**
+     * Initializes class internal state from superglobals and returns an instance o it.
+     *
+     * @return static
+     */
+    public static function instance()
+    {
+        static::initialize();
+
+        return new static();
+    }
 
     /**
      * Initializes class internal state from superglobals.
@@ -177,6 +203,53 @@ final class Globals
     /**
      * Aliases getters and setter for class members.
      */
+    public function __get(string $name)
+    {
+        try {
+            $name   = static::getValidNameOrFail($name);
+            $global = &self::$$name;
+
+            if (isset(static::$globals[$name])) {
+                return static::$globals[$name];
+            }
+
+            return static::$globals[$name] = new class($global) {
+                private $self;
+                public function __construct(&$self)
+                {
+                    $this->self = &$self;
+                }
+                public function get(string $key)
+                {
+                    return Misc::getArrayValueByKey($this->self, $key, null);
+                }
+                public function set(string $key, $value)
+                {
+                    Misc::setArrayValueByKey($this->self, $key, $value);
+
+                    return $this;
+                }
+                public function getAll(): array
+                {
+                    return $this->self;
+                }
+            };
+        } catch (\Exception $error) {
+            throw new \Exception(sprintf('Call to undefined property %s::$%s', static::class, $name), 0, $error);
+        }
+    }
+
+    /**
+     * Allows static methods handled by `self::__callStatic()` to be accessible via object operator `->`.
+     */
+    public function __call(string $method, array $arguments)
+    {
+        return static::__callStatic($method, $arguments);
+    }
+
+    /**
+     * Aliases getters and setter for class members.
+     */
     public static function __callStatic(string $name, array $arguments)
     {
         try {
@@ -189,13 +262,5 @@ final class Globals
         } catch (\Exception $error) {
             throw new \Exception(sprintf('Call to undefined method %s::%s', static::class, $name), 0, $error);
         }
-    }
-
-    /**
-     * Allows static methods handled by self::__callStatic() to be accessible via object operator `->`.
-     */
-    public function __call(string $method, array $arguments)
-    {
-        return static::__callStatic($method, $arguments);
     }
 }
