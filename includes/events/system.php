7583ade8bd4b41e44008fd4792dfbf0fb0bb733d
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-09-16 22:05:47 +0200

    Update events/system.php * Fix some typos

--- includes/events/system.php
+++ includes/events/system.php
@@ -18,40 +18,38 @@
 // * 'config.on.load': This event will be dispatched when the config is loaded. It will be passed a reference to the config array.
 // * 'config.on.cache': This event will be dispatched when the config is cached. It will not be passed any arguments.
 // * 'config.on.clearCache': This event will be dispatched when the config cache is cleared. It will not be passed any arguments.
-// -------------
+
 
 // Controller::class
-// -------------
-// * 'controller.on.construct': This event will be dispatched when a the controller (or a subclass) is constructed. It will not be passed any arguments, but will be bound to the object.
-// -------------
+// -----------------
+// * 'controller.on.construct': This event will be dispatched when a controller (or a subclass) is constructed. It will not be passed any arguments, but will be bound to the object (controller class).
+
 
 // Router::class
 // -------------
-// * 'router.on.registerHandler': This event will be dispatched when a handler is registered. It will be passed a reference to route config array.
-// * 'router.on.registerMiddleware': This event will be dispatched when a middleware is registered. It will be passed a reference to route config array.
-// * 'router.on.start': This event will be dispatched when the router is started. It will be passed a reference to the current parameters.
-// * 'router.before.redirect': This event will be dispatched when a redirect is attempted. It will be passed the redirection path/url.
+// * 'router.on.registerHandler': This event will be dispatched when a handler is registered. It will be passed a reference to the route config array.
+// * 'router.on.registerMiddleware': This event will be dispatched when a middleware is registered. It will be passed a reference to the route config array.
+// * 'router.on.start': This event will be dispatched when the router is started. It will be passed a reference to the router parameters.
+// * 'router.before.redirect': This event will be dispatched when a redirect is attempted. It will be passed the redirection path/URL.
 // * 'router.before.forward': This event will be dispatched when a forward is attempted. It will be passed the forward path.
-// -------------
+
 
 // Data::class
 // -----------
 // * 'data.on.load': This event will be dispatched when the data is loaded. It will be passed a reference to the data array.
-// -----------
+
 
 // View::class
 // -----------
 // * 'view.before.render': This event will be dispatched when a view rendering is attempted. It will be passed a reference to the array that will be passed to the view as variables.
 // * 'view.on.cache': This event will be dispatched when a view is cached. It will not be passed any arguments.
 // * 'view.on.cacheClear': This event will be dispatched when a view cache is cleared. It will not be passed any arguments.
-// -----------
 
 
 
 // Examples
 // --------
 // * Checkout "/storage/logs/events.log" to see the result.
-// --------
 
 Event::listen('config.on.load', function (&$config) {
     App::log('The config was loaded', null, 'events');
