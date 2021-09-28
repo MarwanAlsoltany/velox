31e56e2b0d837ca3b1cb4e811380ec905c347718
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-09-28 23:20:17 +0200

    Update controller test class

--- tests/Backend/ControllerTest.php
+++ tests/Backend/ControllerTest.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace MAKS\Velox\Tests\Backend;
 
 use MAKS\Velox\Tests\TestCase;
+use MAKS\Velox\Tests\Mocks\ControllerMock;
 use MAKS\Velox\Backend\Controller;
 use MAKS\Velox\Backend\Event;
 use MAKS\Velox\Backend\Config;
 use MAKS\Velox\Backend\Router;
 use MAKS\Velox\Backend\Globals;
+use MAKS\Velox\Backend\Session;
+use MAKS\Velox\Backend\Database;
+use MAKS\Velox\Backend\Model;
 use MAKS\Velox\Frontend\Data;
 use MAKS\Velox\Frontend\View;
 use MAKS\Velox\Frontend\HTML;
@@ -24,7 +28,7 @@ class ControllerTest extends TestCase
     {
         parent::setUp();
 
-        $this->controller = new class(['key' => 'value']) extends Controller {};
+        $this->controller = $this->getControllerInstance();
     }
 
     public function tearDown(): void
@@ -38,14 +42,17 @@ class ControllerTest extends TestCase
     public function testControllerPropertiesContainExpectedValues()
     {
         $properties = [
-            'event'   => Event::class,
-            'config'  => Config::class,
-            'router'  => Router::class,
-            'globals' => Globals::class,
-            'data'    => Data::class,
-            'view'    => View::class,
-            'html'    => HTML::class,
-            'path'    => Path::class,
+            'event'    => Event::class,
+            'config'   => Config::class,
+            'router'   => Router::class,
+            'globals'  => Globals::class,
+            'session'  => Session::class,
+            'database' => Database::class,
+            'model'    => Model::class,
+            'data'     => Data::class,
+            'view'     => View::class,
+            'html'     => HTML::class,
+            'path'     => Path::class,
         ];
 
         foreach ($properties as $property => $class) {
@@ -58,5 +65,11 @@ class ControllerTest extends TestCase
         $this->assertIsArray($vars);
         $this->assertArrayHasKey('key', $vars);
         $this->assertEquals('value', $vars['key']);
+    }
+
+
+    private function getControllerInstance(): Controller
+    {
+        return new ControllerMock(['key' => 'value']);
     }
 }
