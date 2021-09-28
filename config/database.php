532508e4440a44049f113c3571d0226e1e955d21
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-09-28 22:46:42 +0200

    Add database config file * Add database.php

---
+++ config/database.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * ---------------------------------------------------------------------------------------------------------------------
+ *  This config file contains database credentials, it is used by the "Database::class".
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
+
+
+
+return [
+
+
+    // Database login info.
+    'driver'   => 'mysql',
+    'host'     => 'localhost',
+    'port'     => 3306,
+    'charset'  => 'utf8mb4',
+    'dbname'   => 'velox',
+    'username' => 'root',
+    'password' => '',
+
+    // Database DSN (Data Source Name) of the connection.
+    // Keep untouched, it will be populated from the provided values above.
+    'dsn'      => '{database.driver}:host={database.host};port={database.port};dbname={database.dbname};charset={database.charset}',
+
+
+];
