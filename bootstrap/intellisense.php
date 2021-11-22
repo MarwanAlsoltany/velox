2cd9ba757a15cd5155b681ca9be2af0cdd61f0fd
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2021-11-22 23:13:14 +0100

    Update intellisense.php * Update file DocBlock * Update classes DocBlock

--- bootstrap/intellisense.php
+++ bootstrap/intellisense.php
@@ -1,24 +1,45 @@
 <?php
 
 /**
- * ---------------------------------------------------------------------------------------------------------------------
- *  This file has no purpose other than to trick IDEs to offer intellisense for dynamically aliased classes.
- * ---------------------------------------------------------------------------------------------------------------------
- *  Please note that this file should not be loaded in the application!
- * ---------------------------------------------------------------------------------------------------------------------
+ * This file has no purpose other than to trick IDEs to offer intellisense for dynamically aliased classes.
+ *
+ * Please note that this file should not be loaded in the application!
  */
 
 
 
+/** Alias */
 class App extends \MAKS\Velox\App {}
+
+/** Alias */
 class Auth extends \MAKS\Velox\Backend\Auth {}
+
+/** Alias */
 class Event extends \MAKS\Velox\Backend\Event {}
+
+/** Alias */
 class Config extends \MAKS\Velox\Backend\Config {}
+
+/** Alias */
 class Router extends \MAKS\Velox\Backend\Router {}
+
+/** Alias */
 class Globals extends \MAKS\Velox\Backend\Globals {}
+
+/** Alias */
 class Session extends \MAKS\Velox\Backend\Session {}
+
+/** Alias */
 class Database extends \MAKS\Velox\Backend\Database {}
+
+/** Alias */
 class View extends \MAKS\Velox\Frontend\View {}
+
+/** Alias */
 class Data extends \MAKS\Velox\Frontend\Data {}
+
+/** Alias */
 class Path extends \MAKS\Velox\Frontend\Path {}
+
+/** Alias */
 class HTML extends \MAKS\Velox\Frontend\HTML {}
