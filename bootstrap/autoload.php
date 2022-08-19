e97d16635aeaf1c95177d927adac426750f25291
Marwan Al-Soltany <MarwanAlsoltany@gmail.com>
2022-08-19 18:20:12 +0200

    Update autoload.php * Add $_VELOX superglobal * CS fixes

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -27,6 +27,11 @@ define('BASE_PATH', dirname(__DIR__));
 
 
 
+// global variables holder for various VELOX global state variables
+$_VELOX = [];
+
+
+
 // load VELOX classes from "../classes/", "../app/" and alias them
 require(BASE_PATH . '/bootstrap/loader.php');
 
@@ -34,22 +39,27 @@ require(BASE_PATH . '/bootstrap/loader.php');
 
 // load composer autoload if composer is used in this project
 $composer = BASE_PATH . '/vendor/autoload.php';
+
 if (file_exists($composer)) {
     require($composer);
 }
+
 unset($composer);
 
 
 // functions are required separately to allow for a variable number of files to be required
 $functions = BASE_PATH . '/functions';
+
 if (file_exists($functions)) {
     require_recursive($functions);
 }
+
 unset($functions);
 
 
 // load additional files defined in "./additional.php"
 $includes = include(BASE_PATH . '/bootstrap/additional.php');
+
 foreach ($includes as $include) {
     if (is_file($include)) {
         require($include);
@@ -58,5 +68,8 @@ foreach ($includes as $include) {
     if (is_dir($include)) {
         require_recursive($include);
     }
+
+    unset($include);
 }
+
 unset($includes);
